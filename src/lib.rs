6bcba51fe93c647ce9a4e26f357d9ade5a0c54a6
Christoph <ignatz@gmail.com>
2017-11-13 13:23:19 -0500

    Added bint and up method

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
+#[allow(dead_code)]
+pub struct Bint {
+    value: u8,
+    boundary: u8,
+}
+
+#[allow(dead_code)]
+impl Bint {
+    fn up(&self) -> Bint {
+        let v = (self.value + 1) % self.boundary;
+        Bint {value: v, boundary: self.boundary}
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn init() {
+        let b = Bint {value: 7, boundary: 10 };
+        assert_eq!(7, b.value);
+        assert_eq!(10, b.boundary);
+    }
+
+    #[test]
+    fn up() {
+        let b: Bint = Bint {value: 0, boundary: 6 };
+        let b: Bint = b.up();
+        assert_eq!(1, b.value);
     }
 }
