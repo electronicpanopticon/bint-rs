73eada0c3989bae7cc434b4b4e826d378997c50f
folkengine <gaoler@electronicpanopticon.com>
2025-09-27 10:17:07 -0700

    Added example

---
+++ examples/double.rs
@@ -0,0 +1,14 @@
+use bint::Bint;
+
+fn perms(i: u8) -> (u8, u8) {
+    (i % 6, i % 10)
+}
+
+fn main() {
+    let mut bint = Bint::new(30);
+    for i in 0..60 {
+        let (x, y) = perms(bint.value);
+        bint = bint.up();
+        println!("{x} {y}");
+    }
+}
