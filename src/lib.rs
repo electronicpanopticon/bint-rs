0380e3ef39350bdf580093982ef1ab29ea715a74
folkengine <gaoler@electronicpanopticon.com>
2025-09-28 06:11:33 -0700

    Finished adding doctests

--- src/lib.rs
+++ src/lib.rs
@@ -28,11 +28,58 @@ pub struct Bint {
 }
 
 impl Bint {
+    /// ```
+    /// use bint::Bint;
+    ///
+    /// let b: Bint = Bint::new(6);
+    /// let c: Bint = b.down();
+    /// let d: Bint = c.up();
+    /// let e: Bint = d.up();
+    ///
+    /// assert_eq!(0, b.value);
+    /// assert_eq!(5, c.value);
+    /// assert_eq!(0, d.value);
+    /// assert_eq!(1, e.value);
+    /// ```
     #[must_use]
     pub fn new(boundary: u8) -> Bint {
         Bint { value: 0, boundary }
     }
 
+    /// ```
+    /// use bint::Bint;
+    ///
+    /// let bint = Bint::new_with_value(10, 7);
+    /// assert_eq!(10, bint.boundary);
+    /// assert_eq!(7, bint.value);
+    ///
+    /// let bint_out_of_range = Bint::new_with_value(10, 23);
+    /// assert_eq!(10, bint_out_of_range.boundary);
+    /// assert_eq!(0, bint_out_of_range.value);
+    /// ```
+    #[must_use]
+    pub fn new_with_value(boundary: u8, value: u8) -> Bint {
+        if value >= boundary {
+            Bint::new(boundary)
+        } else {
+            Bint { value, boundary }
+        }
+    }
+
+    /// ```
+    /// use bint::Bint;
+    ///
+    /// let b: Bint = Bint {
+    ///     value: 4,
+    ///     boundary: 6,
+    /// };
+    ///
+    /// let b: Bint = b.up();
+    /// assert_eq!(5, b.value);
+    ///
+    /// let b: Bint = b.up();
+    /// assert_eq!(0, b.value);
+    /// ```
     #[must_use]
     pub fn up(&self) -> Bint {
         let v = (self.value + 1) % self.boundary;
@@ -42,6 +89,20 @@ impl Bint {
         }
     }
 
+    /// ```
+    /// use bint::Bint;
+    ///
+    /// let b: Bint = Bint {
+    ///     value: 1,
+    ///     boundary: 6,
+    /// };
+    ///
+    /// let b: Bint = b.down();
+    /// assert_eq!(0, b.value);
+    ///
+    /// let b: Bint = b.down();
+    /// assert_eq!(5, b.value);
+    /// ```
     #[must_use]
     pub fn down(&self) -> Bint {
         if self.value == 0 {
@@ -72,6 +133,7 @@ impl fmt::Display for Bint {
 ///
 /// ```
 /// use bint::BintCell;
+///
 /// let b = BintCell::new(6);
 ///
 /// b.down();
@@ -82,13 +144,20 @@ impl fmt::Display for Bint {
 /// b.up();
 /// assert_eq!(2, b.value());
 /// ```
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct BintCell {
     pub cell: Cell<u8>,
     pub boundary: u8,
 }
 
 impl BintCell {
+    /// ```
+    /// use bint::BintCell;
+    ///
+    /// let b = BintCell::new(6);
+    /// assert_eq!(0, b.value());
+    /// assert_eq!(6, b.boundary);
+    /// ```
     #[must_use]
     pub fn new(boundary: u8) -> BintCell {
         BintCell {
@@ -97,6 +166,40 @@ impl BintCell {
         }
     }
 
+    /// ```
+    /// use bint::BintCell;
+    ///
+    /// let b = BintCell::new_with_value(6, 6);
+    /// assert_eq!(0, b.value());
+    /// assert_eq!(6, b.boundary);
+    ///
+    /// let b = BintCell::new_with_value(6, 3);
+    /// assert_eq!(3, b.value());
+    /// assert_eq!(6, b.boundary);
+    /// ```
+    #[must_use]
+    pub fn new_with_value(boundary: u8, value: u8) -> BintCell {
+        if value >= boundary {
+            BintCell::new(boundary)
+        } else {
+            BintCell {
+                cell: Cell::new(value),
+                boundary,
+            }
+        }
+    }
+
+    /// ```
+    /// use bint::BintCell;
+    ///
+    /// let b = BintCell::new(6);
+    ///
+    /// b.up();
+    /// assert_eq!(1, b.value());
+    ///
+    /// b.up();
+    /// assert_eq!(2, b.value());
+    /// ```
     pub fn up(&self) {
         let bint = Bint {
             value: self.value(),
@@ -106,6 +209,17 @@ impl BintCell {
         self.cell.set(bint.value);
     }
 
+    /// ```
+    /// use bint::BintCell;
+    ///
+    /// let b = BintCell::new(6);
+    ///
+    /// b.down();
+    /// assert_eq!(5, b.value());
+    ///
+    /// b.down();
+    /// assert_eq!(4, b.value());
+    /// ```
     pub fn down(&self) {
         let bint = Bint {
             value: self.value(),
@@ -137,9 +251,13 @@ mod tests {
 
     #[test]
     fn new() {
-        let b = Bint::new(6);
-        assert_eq!(0, b.value);
-        assert_eq!(6, b.boundary);
+        assert_eq!(
+            Bint::new(6),
+            Bint {
+                value: 0,
+                boundary: 6
+            }
+        );
     }
 
     #[test]
@@ -149,42 +267,6 @@ mod tests {
             boundary: 6,
         };
         assert_eq!("4", format!("{}", b));
-    }
-
-    #[test]
-    fn init() {
-        let b = Bint {
-            value: 7,
-            boundary: 10,
-        };
-        assert_eq!(7, b.value);
-        assert_eq!(10, b.boundary);
-    }
-
-    #[test]
-    fn up() {
-        let b: Bint = Bint {
-            value: 4,
-            boundary: 6,
-        };
-        let b: Bint = b.up();
-        assert_eq!(5, b.value);
-
-        let b: Bint = b.up();
-        assert_eq!(0, b.value);
-    }
-
-    #[test]
-    fn down() {
-        let b: Bint = Bint {
-            value: 1,
-            boundary: 6,
-        };
-        let b: Bint = b.down();
-        assert_eq!(0, b.value);
-
-        let b: Bint = b.down();
-        assert_eq!(5, b.value);
     }
 
     #[test]
@@ -199,13 +281,6 @@ mod tests {
         let b: Bint = b.down();
         let b: Bint = b.down();
         assert_eq!(9, b.value);
-    }
-
-    #[test]
-    fn cell_new() {
-        let b = BintCell::new(6);
-        assert_eq!(0, b.value());
-        assert_eq!(6, b.boundary);
     }
 
     #[test]
